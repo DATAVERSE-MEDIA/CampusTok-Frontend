25ef4c6cca8f5c102aaf59ad5441d75bc830c3bf
teminoah <dropurmensaje@gmail.com>
2026-01-15 14:47:22 +0100

    fixed the chatbot image size

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -494,8 +494,10 @@ const RightSidebar = ({ navigate }) => (
   <div className="w-full bg-white p-4 lg:p-6 flex items-start sticky top-0 h-fit">
     <div className="border border-gray-200 rounded-lg p-4 lg:p-6 w-full bg-white">
       <div className="flex items-center gap-2 lg:gap-3 mb-3 lg:mb-4">
-        <div className="w-10 h-10 lg:w-12 lg:h-12 bg-blue-600 rounded-full flex items-center justify-center flex-shrink-0">
-          <span className="text-white text-lg lg:text-xl">🤖</span>
+        <div className="w-10 h-10 lg:w-20 lg:h-20 bg-blue-600 rounded-full flex items-center justify-center flex-shrink-0">
+          <span className="text-white text-lg lg:text-xl ">
+             <img src="https://res.cloudinary.com/ddcfjn03w/image/upload/v1768467101/chatbot/chatbot%20icon.png" className=''/>
+          </span>
         </div>
         <div>
           <h3 className="font-bold text-gray-900 text-sm lg:text-base">HI, I'm ChatBot</h3>
